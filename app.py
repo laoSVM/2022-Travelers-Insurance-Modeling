0c223485f7abcaef9840fdaf45fc24c56de6ca79
SVM <66192765+laoSVM@users.noreply.github.com>
2022-11-25 23:59:33 +0000

    reset

--- app.py
+++ app.py
@@ -113,19 +113,14 @@ def main():
         tsQuest = st.selectbox('More detailed analysis', tsQuests)
 
         if tsQuest == tsQuests[0]:
-            def reset_slider():
-                st.session_state['time_range'][0] = date(2015,1,1)
-                st.session_state['time_range'][1] = date(2019,1,31)
-                # st.write(st.session_state['time_range'][0])
-                # st.write(st.session_state['time_range'][1])
-                # st.markdown('---')
+
             # if not specified: whole time series
-            st.slider(
+            start, end = st.slider(
                 "Pick a date range of interest:",
                 value=(date(2015,1,1), date(2019,1,31)),
                 key='time_range')
-            start = st.session_state['time_range'][0].strftime('%Y%m%d')
-            end = st.session_state['time_range'][1].strftime('%Y%m%d')
+            start = start.strftime('%Y%m%d')
+            end = end.strftime('%Y%m%d')
             # if specified time range: cal based on time range
             df = query_ts_data(resample='M', query=f'Quote_dt >={start} and Quote_dt <= {end}')
             fig = px.line(df, x=df.index, y='cov_rate',
@@ -135,7 +130,7 @@ def main():
                 })
             st.plotly_chart(fig)
             
-            st.button('Reset', on_click=reset_slider)
+
         
         if tsQuest == tsQuests[1]:
             pass
