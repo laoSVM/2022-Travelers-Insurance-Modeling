7d15f2b95b0098b3626a9dbf24e25cc32c336895
SVM <66192765+laoSVM@users.noreply.github.com>
2022-12-13 01:24:54 +0000

    update

--- app.py
+++ app.py
@@ -144,7 +144,7 @@ def main():
                 current_cov = pd.DataFrame({'sum': np.sum(current_record), 'count': current_record.size}).assign(cov_rate = lambda x: x['sum']/x['count'])
                 prev_record = get_ts_data().loc[lambda x: (x.Quote_dt<=prev_month) & (x.Quote_dt>=two_month_before), ['convert_ind']]
                 prev_cov = pd.DataFrame({'sum': np.sum(prev_record), 'count': prev_record.size}).assign(cov_rate = lambda x: x['sum']/x['count'])
-                st.write(current_cov, prev_cov)
+                st.write(current_cov, prev_cov, prev_month, two_month_before)
                 st.metric('Conversion', f"{(current_cov['cov_rate'].values[0]):.2%}", f"{( (current_cov['cov_rate'].values[0] - prev_cov['cov_rate'].values[0]) / prev_cov['cov_rate'].values[0]) :.2%}")
                 st.metric('Num Quotes', current_cov['count'], f"{( (current_cov['count'].values[0] - prev_cov['count'].values[0]) / prev_cov['count'].values[0]):.2%}")
             with right:
